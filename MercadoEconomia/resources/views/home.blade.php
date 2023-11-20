a8027ce60de7bf01493bc0d66337c668ade59308
GabrielSoaresCeravolo <gabriel.striderws@gmail.com>
2023-11-20 18:43:57 -0300

    a slight improvement was made to the frontend

--- MercadoEconomia/resources/views/home.blade.php
+++ MercadoEconomia/resources/views/home.blade.php
@@ -9,85 +9,58 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-
-<div id="carouselExample" class="carousel slide">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-
 <div class="container1">
-    <div class="card-container">
-            <div class="card" style="width: 18rem;">
-                <img src="/img/bn1.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Banana</h5>
-                    <a href="/estoque" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div class="card" style="width: 18rem;">
-                <img src="/img/maça.JPEG" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Maçã</h5>                    
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
+  <div class="card-container">
 
-            <div class="card" style="width: 18rem;">
-                <img src="/img/pk.JPEG" class="card-img-top" alt="...">
-                    <div class="card-body">
-                    <h5 class="card-title">Carne</h5>
-                    <a href="###########" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        
+    <div class="card">
+      <img src="/img/bn1.jpg" class="card-img-top" alt="banana" class="color: #dc3545;">
+      <div class="card-body text-center">
+        <h5 class="card-title">Banana</h5>
+        <a href="#" class="btn btn-secondary custom-btn button-card">Visualizar</a>
+      </div>
+    </div>
 
-        <div class="card" style="width: 18rem;">
-                <img src="/img/dt.JPEG" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Detergente Ype</h5>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
+    <div class="card">
+      <img src="/img/maça.JPEG" class="card-img-top" alt="maça">
+      <div class="card-body text-center">
+        <h5 class="card-title">Maçã</h5>
+        <a href="#" class="btn btn-secondary custom-btn button-card">Visualizar</a>
+      </div>
+    </div>
 
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        </div>
+    <div class="card">
+      <img src="/img/pk.JPEG" class="card-img-top" alt="pk">
+      <div class="card-body text-center">
+        <h5 class="card-title">Carne</h5>
+        <a href="#" class="btn btn-secondary custom-btn button-card">Visualizar</a>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src="/img/cocaJACKDANIELS.JPEG" class="card-img-top" alt="coca">
+      <div class="card-body text-center">
+        <h5 class="card-title">Coca Deniels</h5>
+        <a href="#" class="btn btn-secondary custom-btn button-card">Visualizar</a>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src="/img/fogao.JPEG" class="card-img-top" alt="fogao">
+      <div class="card-body text-center">
+        <h5 class="card-title">Fogão</h5>
+        <a href="#" class="btn btn-secondary custom-btn button-card">Visualizar</a>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src="/img/tv.webp" class="card-img-top" alt="tv">
+      <div class="card-body text-center">
+        <h5 class="card-title">Televisão</h5>
+        <a href="#" class="btn btn-secondary custom-btn button-card">Visualizar</a>
+      </div>
+    </div>
+
+  </div>
 </div>
-
-<div class="alert alert-primary" style="display: flex; justify-content: center; align-items: center; margin: top 10%;" role="alert">
-        Site desenvolvido apenas para aprendizagem do laravel, não foi trabalhado um desing!
-</div>
-,
 
 @endsection
